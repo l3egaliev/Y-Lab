a0f79c7af7df784fad2d7571f44e11f85db54e5f
Rocky <tedrocky@gmail.com>
2024-01-27 20:29:21 +0300

    JavaDoc: для Audit

--- src/main/java/kg/rakhim/classes/models/Audit.java
+++ src/main/java/kg/rakhim/classes/models/Audit.java
@@ -1,3 +1,16 @@
+/**
+ * Класс {@code Audit} представляет аудитовую запись о действиях пользователей в системе.
+ * <p>
+ * Имеет поля для хранения имени пользователя, выполненного действия и времени его выполнения.
+ * </p>
+ * <p>
+ * Реализованы геттеры, сеттеры, конструктор и переопределен метод {@code toString()} для удобного представления информации.
+ * </p>
+ *
+ * @author Рахим Нуралиев
+ * @version 1.0
+ * @since 2024-01-26
+ */
 package kg.rakhim.classes.models;
 
 import lombok.AllArgsConstructor;
@@ -6,6 +19,9 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+/**
+ * Класс {@code Audit} представляет аудитовую запись о действиях пользователей в системе.
+ */
 @Setter
 @Getter
 @AllArgsConstructor
@@ -14,7 +30,11 @@ public class Audit {
     private String action;
     private LocalDateTime time;
 
-
+    /**
+     * Переопределение метода {@code toString()} для удобного представления информации об аудитовой записи.
+     *
+     * @return строковое представление объекта {@code Audit}
+     */
     @Override
     public String toString() {
         return "Audit{" +
