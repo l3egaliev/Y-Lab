830fd92219ba4b355c587221ec4d087e43907bdd
Rocky <tedrocky@gmail.com>
2024-01-27 20:26:21 +0300

    JavaDoc: для RegisterService

--- src/main/java/kg/rakhim/classes/in/RegisterService.java
+++ src/main/java/kg/rakhim/classes/in/RegisterService.java
@@ -1,3 +1,19 @@
+/**
+ * Класс {@code RegisterService} предоставляет сервис для регистрации и авторизации пользователей.
+ * <p>
+ * Зависит от класса {@link Storage} для доступа и манипуляции данными о пользователях и аудитах.
+ * </p>
+ * <p>
+ * Реализует методы для регистрации нового пользователя и проверки аутентификации.
+ * </p>
+ *
+ * @author Рахим Нуралиев
+ * @version 1.0
+ * @since 2024-01-26
+ * @see Storage
+ * @see Audit
+ * @see User
+ */
 package kg.rakhim.classes.in;
 
 import kg.rakhim.classes.database.Storage;
@@ -6,16 +22,27 @@ import kg.rakhim.classes.models.User;
 
 import java.time.LocalDateTime;
 
-
+/**
+ * Класс {@code RegisterService} предоставляет сервис для регистрации и авторизации пользователей.
+ */
 public class RegisterService {
     private final Storage storage;
 
-
+    /**
+     * Конструктор для создания экземпляра класса {@code RegisterService}.
+     *
+     * @param storage объект Storage для доступа и манипуляции данными о пользователях и аудитах
+     */
     public RegisterService(Storage storage) {
         this.storage = storage;
     }
 
-
+    /**
+     * Регистрация нового пользователя.
+     *
+     * @param user объект User для регистрации
+     * @return true, если регистрация успешна; false, если пользователь уже существует
+     */
     public boolean registerUser(User user) {
         boolean res = true;
         // Проверка - не существует ли такого пользователя
@@ -31,7 +58,13 @@ public class RegisterService {
         return res;
     }
 
-
+    /**
+     * Авторизация пользователя.
+     *
+     * @param username имя пользователя
+     * @param password пароль пользователя
+     * @return true, если пользователь аутентифицирован; false, если не найден или пароль неверен
+     */
     public boolean loginUser(String username, String password) {
         boolean res = false;
         for (User u : storage.getUsers()) {
