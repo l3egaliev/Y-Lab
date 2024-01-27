873ea739a08087b878fb533799210640952f41a7
Rocky <tedrocky@gmail.com>
2024-01-27 20:28:40 +0300

    JavaDoc: для MeterReading

--- src/main/java/kg/rakhim/classes/models/MeterReading.java
+++ src/main/java/kg/rakhim/classes/models/MeterReading.java
@@ -1,3 +1,17 @@
+/**
+ * Класс {@code MeterReading} представляет собой объект, содержащий информацию о показаниях счетчика.
+ * <p>
+ * Имеет поля для хранения пользователя, значения показаний, типа счетчика и даты подачи показаний.
+ * </p>
+ * <p>
+ * Реализованы геттеры, сеттеры и переопределен метод {@code toString()} для удобного представления информации.
+ * </p>
+ *
+ * @author Рахим Нуралиев
+ * @version 1.0
+ * @since 2024-01-26
+ * @see User
+ */
 package kg.rakhim.classes.models;
 
 import lombok.Getter;
@@ -5,7 +19,9 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 
-
+/**
+ * Класс {@code MeterReading} представляет объект, содержащий информацию о показаниях счетчика.
+ */
 @Getter
 @Setter
 public class MeterReading {
@@ -14,6 +30,10 @@ public class MeterReading {
     private String meterType;
     private LocalDate date;
 
+    /**
+     * Конструктор - используется в объекте Storage для создания три default типа счетчиков
+     * другие переменные заполняем для того чтобы не было ошибки NullPointerException.
+     */
     public MeterReading(String t){
         this.meterType = t;
         this.user = new User("default");
@@ -23,6 +43,11 @@ public class MeterReading {
 
     public MeterReading(){}
 
+    /**
+     * Переопределение метода {@code toString()} для удобного представления информации о показаниях счетчика.
+     *
+     * @return строковое представление объекта {@code MeterReading}
+     */
     @Override
     public String toString() {
         return "Показание{" +
