2a78b6e34a7bdfb2a12f4524cc5f739ab71706be
Rocky <tedrocky@gmail.com>
2024-01-27 20:30:18 +0300

    JavaDoc: для User

--- src/main/java/kg/rakhim/classes/models/User.java
+++ src/main/java/kg/rakhim/classes/models/User.java
@@ -1,3 +1,16 @@
+/**
+ * Класс {@code User} представляет объект пользователя системы.
+ * <p>
+ * Имеет поля для хранения имени пользователя, пароля и роли пользователя.
+ * </p>
+ * <p>
+ * Реализованы геттеры, сеттеры, конструкторы и переопределен метод {@code toString()} для удобного представления информации.
+ * </p>
+ *
+ * @author Рахим Нуралиев
+ * @version 1.0
+ * @since 2024-01-26
+ */
 package kg.rakhim.classes.models;
 
 import lombok.AllArgsConstructor;
@@ -5,7 +18,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+/**
+ * Класс {@code User} представляет объект пользователя системы.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,10 +30,18 @@ public class User {
     private String password;
     private UserRole role;
 
+    /**
+     * Конструктор, который используется в объекте MeterReading
+     */
     public User(String username){
         this.username = username;
     }
 
+    /**
+     * Переопределение метода {@code toString()} для удобного представления информации о пользователе.
+     *
+     * @return строковое представление объекта {@code User}
+     */
     @Override
     public String toString() {
         return username;
